c1da4b8e7c7a23f1ce2c20090a2b6460477f535d
schei <schei@host.docker.internal>
2026-04-28 14:22:45 -0500

    2/3 of the filer

--- src/java/filter/SecurityFilter.java
+++ src/java/filter/SecurityFilter.java
@@ -1,11 +1,13 @@
 package filter;
 
 import java.io.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 /**
- *
  * @author schei
  */
 public class SecurityFilter implements Filter {
@@ -13,18 +15,39 @@ public class SecurityFilter implements Filter {
     private FilterConfig filterConfig = null;
 
     @Override
-    public void init(FilterConfig filterConfig)
-    {
+    public void init(FilterConfig filterConfig) {
         this.filterConfig = filterConfig;
     }
 
     @Override
-    public void doFilter(ServletRequest request,
+    public void doFilter(
+            ServletRequest request,
             ServletResponse response,
-            FilterChain chain)
-            throws IOException, ServletException
-    {
+            FilterChain chain) throws IOException, ServletException {
+
+        chain.doFilter(request, response);
         
+        HttpServletRequest httpRequest
+                = (HttpServletRequest) request;
+        
+        HttpServletResponse httpResponse
+                = (HttpServletResponse) response;
+        
+        ServletContext sc = filterConfig.getServletContext();
+        
+        String logString = filterConfig.getFilterName() + " | ";
+        
+        logString += "Servlet path: "
+                + httpRequest.getServletPath() + " | ";
+        
+        logString += "Content type: "
+                + httpResponse.getContentType();
+        
+        sc.log(logString);
     }
 
+    @Override
+    public void destroy() {
+        filterConfig = null;
+    }
 }
